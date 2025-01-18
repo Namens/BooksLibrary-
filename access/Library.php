70c5c7bce8f2b5483611e4e04e599d3d1e31ce07
Daniil <131454347+Namens@users.noreply.github.com>
2025-01-18 13:21:13 +0500

    FirstCommit

---
+++ access/Library.php
@@ -0,0 +1,36 @@
+<?php
+
+class Library {
+    private $books;
+    private $readers;
+
+    public function __construct() {
+        $this->books = [];
+        $this->readers = [];
+    }
+
+    public function addBook($book) {
+        $this->books[] = $book;
+    }
+
+    public function addReader($reader) {
+        $this->readers[] = $reader;
+    }
+
+    public function findBook($title) {
+        foreach ($this->books as $book) {
+            if ($book->getTitle() === $title) {
+                return $book;
+            }
+        }
+        return null;
+    }
+
+    public function listBooks() {
+        foreach ($this->books as $book) {
+            echo "<b>Название:</b> " . $book->getTitle() . ", <b>Автор:</b> " . $book->getAuthor() . "<br/>";
+        }
+    }
+}
+
+?>
